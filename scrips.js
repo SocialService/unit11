9f9536a9c9dee5bc2274e1a3bc002c5b4b4ce6e1
JevGor <reawqk@mail.ru>
2019-05-02 21:50:14 +0200

    Create scrips.js

---
+++ scrips.js
@@ -0,0 +1,33 @@
+var elem = document.querySelector("#elem")
+var row = document.getElementById("row")
+var colom = document.getElementById("colom")
+var button = document.getElementById("button")
+
+button.addEventListener('click', function (el, rows, cols) {
+  var el = elem
+  var rows = parseInt(row.value)
+  var cols = parseInt(colom.value)
+  var table = document.createElement('table')
+
+  for (var i = 0; i < rows; i += 1) {
+    var tr = document.createElement('tr');
+
+    for (var x = 0; x < cols; x += 1) {
+      var td = document.createElement('td');
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+
+  el.appendChild(table);
+})
+
+elem.addEventListener('click', function (cell) {
+  var cell = document.getElementsByTagName('td') 
+  var chip = document.createElement('div')
+  chip.className = 'chip'
+  chip.id = 'chip'
+  cell.appendChild(div)
+})
+
+
